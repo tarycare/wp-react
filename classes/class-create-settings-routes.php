33e934dd0d1112a75ed6f70b6e23c74c95d47aa5
abusiraj84 <abusiraj84@gmail.com>
2024-09-03 15:59:33 +0300

    1

--- classes/class-create-settings-routes.php
+++ classes/class-create-settings-routes.php
@@ -1,55 +1,75 @@
 <?php
+
 /**
  * This file will create Custom Rest API End Points.
  */
-class WP_React_Settings_Rest_Route {
+class WP_React_Settings_Rest_Route
+{
 
-    public function __construct() {
-        add_action( 'rest_api_init', [ $this, 'create_rest_routes' ] );
+    public function __construct()
+    {
+        add_action('rest_api_init', [$this, 'create_rest_routes']);
     }
 
-    public function create_rest_routes() {
-        register_rest_route( 'wprk/v1', '/settings', [
+    public function create_rest_routes()
+    {
+        register_rest_route('wprk/v1', '/settings', [
             'methods' => 'GET',
-            'callback' => [ $this, 'get_settings' ],
-            'permission_callback' => [ $this, 'get_settings_permission' ]
-        ] );
-        register_rest_route( 'wprk/v1', '/settings', [
+            'callback' => [$this, 'get_settings'],
+            'permission_callback' => [$this, 'get_settings_permission']
+        ]);
+        register_rest_route('doc/v1', '/add', [
             'methods' => 'POST',
-            'callback' => [ $this, 'save_settings' ],
-            'permission_callback' => [ $this, 'save_settings_permission' ]
-        ] );
+            'callback' => [$this, 'add_doc'],
+            'permission_callback' => [$this, 'save_settings_permission']
+        ]);
     }
 
-    public function get_settings() {
-        $firstname = get_option( 'wprk_settings_firstname' );
-        $lastname  = get_option( 'wprk_settings_lastname' );
-        $email     = get_option( 'wprk_settings_email' );
+    public function get_settings()
+    {
+        $firstname = get_option('wprk_settings_firstname');
+        $lastname  = get_option('wprk_settings_lastname');
+        $email     = get_option('wprk_settings_email');
         $response = [
             'firstname' => $firstname,
             'lastname'  => $lastname,
             'email'     => $email
         ];
 
-        return rest_ensure_response( $response );
+        return rest_ensure_response($response);
     }
 
-    public function get_settings_permission() {
+    public function get_settings_permission()
+    {
         return true;
     }
 
-    public function save_settings( $req ) {
-        $firstname = sanitize_text_field( $req['firstname'] );
-        $lastname  = sanitize_text_field( $req['lastname'] );
-        $email     = sanitize_text_field( $req['email'] );
-        update_option( 'wprk_settings_firstname', $firstname );
-        update_option( 'wprk_settings_lastname', $lastname );
-        update_option( 'wprk_settings_email', $email );
-        return rest_ensure_response( 'success' );
+    public function add_doc($req)
+    {
+
+        // it will uplade custom post type image_url doc_type moudle_type
+
+        $request = json_decode($req->get_body());
+        $doc_type = $request->doc_type;
+        $moudle_type = $request->moudle_type;
+        $image_url = $request->image_url;
+        $post_id = wp_insert_post(array(
+            'post_title' => $doc_type,
+            'post_content' => $moudle_type,
+            'post_status' => 'publish',
+            'post_type' => 'doc',
+        ));
+        if ($post_id) {
+            update_post_meta($post_id, 'image_url', $image_url);
+            return rest_ensure_response('Document Added Successfully');
+        } else {
+            return rest_ensure_response('Error in Adding Document');
+        }
     }
 
-    public function save_settings_permission() {
-        return current_user_can( 'publish_posts' );
+    public function save_settings_permission()
+    {
+        return current_user_can('publish_posts');
     }
 }
 new WP_React_Settings_Rest_Route();
--- src/index.js
+++ src/index.js
@@ -1,15 +1,76 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; // Import createRoot from react-dom/client
-import "./style.css";
+// i want a + button for new document upload when click it will open in table row that have 3 column 1st is upload input file 2nd file type public or private 3d mdoule type dropdown list that have 3 options 1st Employee 2nd Customer 3rd Vendor , i can add multiple row and when i click on New Document button it will add new row in table and make style by tailwind css
+// i want a - button for delete row when click it will delete that row
+// i want a save button when click it will save all data in table in json format and show in console
+// i want a clear button when click it will clear all data in table
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
+  const [rows, setRows] = useState([{ id: 1 }]);
+
+  const addRow = () => {
+    setRows([...rows, { id: rows.length + 1 }]);
+  };
+
+  const deleteRow = (id) => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const saveData = () => {
+    console.log(rows);
+    // fetch post to wordpress api to save data
+  };
+
+  const clearData = () => {
+    setRows([]);
+  };
+
   return (
-    <div className="bg-white size-[800px] rounded-md p-2">
-      <h1 className="text-2xl font-bold text-center">Hello World</h1>
-      <p className="text-center">This is a React app in WordPress</p>
+    <div>
+      <button onClick={addRow}>New Document</button>
+      <button onClick={saveData}>Save</button>
+      <button onClick={clearData}>Clear</button>
+      <table className="border border-1 border-black">
+        <thead>
+          <tr>
+            <th>Upload Input File</th>
+            <th>File Type</th>
+            <th>Module Type</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row) => (
+            <tr key={row.id}>
+              <td>
+                <input type="file" />
+              </td>
+              <td>
+                <select>
+                  <option value="public">Public</option>
+                  <option value="private">Private</option>
+                </select>
+              </td>
+              <td>
+                <select>
+                  <option value="employee">Employee</option>
+                  <option value="customer">Customer</option>
+                  <option value="vendor">Vendor</option>
+                </select>
+              </td>
+              <td>
+                <button onClick={() => deleteRow(row.id)}>-</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
+
+export default App;
 
 // Get the DOM element where your app will be rendered
 const rootElement = document.getElementById("wp-react-app");
