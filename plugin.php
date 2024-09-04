3d4debc7094e3e3976e64ace9852371bf601683b
abusiraj84 <abusiraj84@gmail.com>
2024-09-04 22:33:34 +0300

    1

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP React KickOff
  * Author: Husam nasrallah
  * Author URI: https://github.com/robicse11127
- * Version: 1.0.9
+ * Version: 1.0.10
  * Description: WordPress React KickOff.
  * Text-Domain: wp-react-kickoff
  * GitHub Plugin URI: abusiraj84/wp-react
