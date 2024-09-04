e636fbf11d0189cf086cccdfdf7595fd1c7cfa7f
abusiraj84 <abusiraj84@gmail.com>
2024-09-05 00:28:27 +0300

    1

--- plugin.php
+++ plugin.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Plugin Name: WP React KickOff
+ * Plugin Name: WP React
  * Author: Husam nasrallah
  * Author URI: https://github.com/robicse11127
  * Version: 1.0.12
- * Description: WordPress React KickOff.
- * Text-Domain: wp-react-kickoff
+ * Description: WordPress React
+ * Text-Domain: wp-react
  * GitHub Plugin URI: abusiraj84/wp-react
  * GitHub Plugin URI: https://github.com/abusiraj84/wp-react
 
@@ -32,15 +32,15 @@ function load_scripts()
 
     if ($is_dev) {
         // Load from Webpack Dev Server
-        wp_enqueue_script('wp-react-kickoff', 'http://localhost:3000/dist/bundle.js', ['jquery', 'wp-element'], wp_rand(), true);
-        wp_enqueue_style('wp-react-kickoff-style', 'http://localhost:3000/dist/style.css', [], wp_rand());
+        wp_enqueue_script('wp-react-', 'http://localhost:3000/dist/bundle.js', ['jquery', 'wp-element'], wp_rand(), true);
+        wp_enqueue_style('wp-react-style', 'http://localhost:3000/dist/style.css', [], wp_rand());
     } else {
         // Load the production bundle from the plugin directory
-        wp_enqueue_script('wp-react-kickoff', WPRK_URL . 'dist/bundle.js', ['jquery', 'wp-element'], wp_rand(), true);
-        wp_enqueue_style('wp-react-kickoff-style', WPRK_URL . 'dist/style.css', [], wp_rand());
+        wp_enqueue_script('wp-react', WPRK_URL . 'dist/bundle.js', ['jquery', 'wp-element'], wp_rand(), true);
+        wp_enqueue_style('wp-react-style', WPRK_URL . 'dist/style.css', [], wp_rand());
     }
 
-    wp_localize_script('wp-react-kickoff', 'appLocalizer', [
+    wp_localize_script('wp-react', 'appLocalizer', [
         'apiUrl' => home_url('/wp-json'),
         'nonce' => wp_create_nonce('wp_rest'),
     ]);
